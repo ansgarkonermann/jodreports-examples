a318b061fbd9a9bbc95711b91525b9de69aaa00b
Ansgar Konermann <ansgar.konermann@googlemail.com>
2011-05-12 18:16:59 +0200

    Extended learning test: how to generate documents from streams, allowing to connect to OpenOffice running on different host.

--- learningtests/src/test/java/de/lightful/experiments/jodreports/TestDocumentGeneration.java
+++ learningtests/src/test/java/de/lightful/experiments/jodreports/TestDocumentGeneration.java
@@ -1,15 +1,25 @@
 package de.lightful.experiments.jodreports;
 
+import com.artofsolving.jodconverter.DefaultDocumentFormatRegistry;
 import com.artofsolving.jodconverter.DocumentConverter;
+import com.artofsolving.jodconverter.DocumentFormat;
+import com.artofsolving.jodconverter.DocumentFormatRegistry;
 import com.artofsolving.jodconverter.openoffice.connection.OpenOfficeConnection;
 import com.artofsolving.jodconverter.openoffice.connection.SocketOpenOfficeConnection;
 import com.artofsolving.jodconverter.openoffice.converter.OpenOfficeDocumentConverter;
+import com.artofsolving.jodconverter.openoffice.converter.StreamOpenOfficeDocumentConverter;
 import net.sf.jooreports.templates.DocumentTemplate;
 import net.sf.jooreports.templates.DocumentTemplateFactory;
+import org.apache.commons.io.output.NullOutputStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.Test;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.math.BigDecimal;
 import java.net.URL;
 import java.util.Arrays;
@@ -18,9 +28,14 @@ import java.util.List;
 import java.util.Map;
 
 import static de.lightful.experiments.jodreports.LoanPartType.*;
+import static org.fest.assertions.Assertions.assertThat;
 
 @Test
 public class TestDocumentGeneration {
+
+  public static final int SPEED_TEST_ITERATIONS = 100;
+
+  private Logger logger = LoggerFactory.getLogger(TestDocumentGeneration.class);
 
   private List<Item> createCartItems() {
     return Arrays.asList(
@@ -57,7 +72,6 @@ public class TestDocumentGeneration {
     template.createDocument(dataModel, new FileOutputStream("document-instance.odt"));
   }
 
-
   @Test
   public void testConvertToPdf() throws Exception {
     OpenOfficeConnection connection = null;
@@ -77,7 +91,78 @@ public class TestDocumentGeneration {
     }
     finally {
       // close the connection
-      connection.disconnect();
+      if (connection.isConnected()) {
+        connection.disconnect();
+      }
+    }
+  }
+
+  @Test
+  public void testConvertToPdfUsingStreams() throws Exception {
+    OpenOfficeConnection connection = null;
+    try {
+      File inputFile = new File("document-instance.odt");
+      File outputFile = new File("document-instance.pdf");
+      InputStream inputStream = new FileInputStream(inputFile);
+      OutputStream outputStream = new FileOutputStream(outputFile);
+
+      // connect to an OpenOffice.org instance running on port 8100
+      // run in shell: soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard
+      // See: http://www.artofsolving.com/
+      connection = new SocketOpenOfficeConnection("dev.emma-nl.hypoport.local", 8100);
+      connection.connect();
+
+      // convert
+      final DefaultDocumentFormatRegistry formatRegistry = new DefaultDocumentFormatRegistry();
+      DocumentConverter converter = new StreamOpenOfficeDocumentConverter(connection, formatRegistry);
+      final DocumentFormat odtFileFormat = formatRegistry.getFormatByFileExtension("odt");
+      final DocumentFormat pdfFileFormat = formatRegistry.getFormatByFileExtension("pdf");
+      converter.convert(inputStream, odtFileFormat, outputStream, pdfFileFormat);
+    }
+    finally {
+      // close the connection
+      if (connection.isConnected()) {
+        connection.disconnect();
+      }
+    }
+  }
+
+  @Test
+  public void testConversionSpeedWithStreams() throws Exception {
+    OpenOfficeConnection connection = null;
+    try {
+      File inputFile = new File("document-instance.odt");
+      File outputFile = new File("document-instance.pdf");
+
+      // connect to an OpenOffice.org instance running on port 8100
+      // run in shell: soffice -headless -accept="socket,host=127.0.0.1,port=8100;urp;" -nofirststartwizard
+      // See: http://www.artofsolving.com/
+      connection = new SocketOpenOfficeConnection("127.0.0.1", 8100);
+      connection.connect();
+      assertThat(connection.isConnected()).as("Connection status 'connected'?").isTrue();
+
+      // convert
+      final DefaultDocumentFormatRegistry formatRegistry = new DefaultDocumentFormatRegistry();
+      DocumentConverter converter = new StreamOpenOfficeDocumentConverter(connection, formatRegistry);
+      final DocumentFormat odtFileFormat = formatRegistry.getFormatByFileExtension("odt");
+      final DocumentFormat pdfFileFormat = formatRegistry.getFormatByFileExtension("pdf");
+      final long timestampAtStart = System.currentTimeMillis();
+      for (int i = 0; i < SPEED_TEST_ITERATIONS; i++) {
+        InputStream inputStream = new FileInputStream(inputFile);
+        OutputStream outputStream = new NullOutputStream();
+        converter.convert(inputStream, odtFileFormat, outputStream, pdfFileFormat);
+      }
+      final long timestampAtEnd = System.currentTimeMillis();
+
+      final long duration = timestampAtEnd - timestampAtStart;
+      logger.info("Duration: " + duration + "ms (=> " + duration * 1.0d / SPEED_TEST_ITERATIONS + "ms per document generation)");
+
+    }
+    finally {
+      // close the connection
+      if (connection.isConnected()) {
+        connection.disconnect();
+      }
     }
   }
 }
